2bee9e57479ff93270cb5737986a199e7bdfa7f5
Manabendra nath Saren <97617754+ManabendranathSaren@users.noreply.github.com>
2026-03-13 01:51:19 +0530

    Enhance character creation validation and output
    Added validation for character name and stats, and updated print statements to show character attributes.

--- RPG Character/RPG Character.py
+++ RPG Character/RPG Character.py
@@ -4,17 +4,17 @@ empty_dot = '○'
 def create_character(NAME, STRE, INTE, CHAR):
     if not isinstance(NAME, str):
         return 'The character name should be a string'
-    if not isinstance(NAME, ""):
+    if len(NAME) == 0:
         return 'The character should have a name'
+    if " " in NAME:
+        return 'The character name should not contain spaces'
     if len(NAME) > 10:
         return 'The character name is too long'
-    if x in NAME == " ":
-        return 'The character name should not contain spaces'
-    if not isinstance(STRE, INTE):
+    if not isinstance(STRE, int):
         return 'All stats should be integers'
-    if not isinstance(INTE, INTE):
+    if not isinstance(INTE, int):
         return 'All stats should be integers'
-    if not isinstance(CHAR, INTE):
+    if not isinstance(CHAR, int):
         return 'All stats should be integers'
     if STRE < 1 or CHAR < 1 or INTE < 1 :
         return 'All stats should be no less than 1'
@@ -23,8 +23,26 @@ def create_character(NAME, STRE, INTE, CHAR):
     if (STRE + INTE + CHAR) != 7 :
         return "The character should start with 7 points"
     
+    STRE = STRE*full_dot + (10-STRE)*empty_dot
+    INTE = INTE*full_dot + (10-INTE)*empty_dot
+    CHAR = CHAR*full_dot + (10-CHAR)*empty_dot
+    print("NAME ", NAME)
+    print("STRE ", STRE)
+    print("INTE ", INTE)
+    print("CHAR ", CHAR)
+
+create_character("Manab",4,2,1)
     
     print("NAME ", NAME)
     print("STRE ", STRE)
     print("INTE ", INTE)
     print("CHAR ", CHAR)
+
+
+# Output
+'''
+NAME  Manab
+STRE  ●●●●○○○○○○
+INTE  ●●○○○○○○○○
+CHAR  ●○○○○○○○○○
+'''
